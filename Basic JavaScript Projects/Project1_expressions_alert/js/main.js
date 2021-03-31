4f3b9e280ca3bf3dbdbe48fcab2d8b494e4ed1d4
Chris <79873408+govtchris@users.noreply.github.com>
2021-03-31 13:58:43 -0500

    Add Comments

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,34 @@
+//Writing a string
+document.write("Hello, World!");
+
+//Assigning a string to a variable
+var A = "This is a string";
+document.write(A)
+
+//Assigning a variable to an alert
+var B = "Howdy Howdy Howdy";
+window.alert(B);
+
+//Using backslashes to add quotations
+document.write("\"When you brake the plate, you don't fix it, you just get another plate.\", David Haller ")
+
+//Concatenating strings in JavaScript
+document.write("\"Human beings are the only animal that forms ideas about their world." 
++ "We perceive it not through our bodies but through our minds. "
++ "We must agree on what is real. "
++ "Because of this, we are the only animal on Earth that goes mad.\" ")
+
+//Assigning multiple variables at once, using the items I see
+var Desk = "Brown", Pierre = "Brown and White",
+    Frog = "Green", Ice_Giant = "Blue";
+
+document.write(Pierre);
+
+//Chaning the font color on an assigned variable
+var frieza = "You seem awfully confident in yourself";
+var frieza = frieza.fontcolor("purple")
+document.write(frieza);
+
+//Writing an expression
+document.write(68*9)
+
