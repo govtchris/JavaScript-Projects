768b404a62a87e527b95925b511fb0720bc482fb
Chris <79873408+govtchris@users.noreply.github.com>
2021-04-02 14:26:06 -0500

    Update main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -29,3 +29,34 @@ function negdis_Infinity() {
     document.getElementById("NegInfinite").innerHTML = (-2e310)
 }
     
+//Boolean
+document.write(3<5);
+document.write(3>5);
+
+//Console Log
+console.log(87*87);
+console.log(87<87);
+
+//Double Equal
+document.write(12==12);
+document.write(12==87);
+
+//Triple Equal True Data Type and Value
+X = 87;
+Y = 87;
+document.write(X===Y)
+
+//Triple Equal Match Different Data Type and Value
+X = 87;
+Y = "Pierre";
+document.write(X===Y)
+
+//Triple Equal Different Data Type and  Same Value
+X = 87;
+Y = "87";
+document.write(X===Y)
+
+//Triple Equal True Same Data Type and Different Value
+X = 87;
+Y = 78;
+document.write(X===Y)
